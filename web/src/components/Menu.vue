371dba035293efe3ae9d8f08f905b7356224cb3d
Kirill <kkovaluk11@gmail.com>
2025-05-05 14:11:42 +0300

    add stile registration

--- web/src/components/Menu.vue
+++ web/src/components/Menu.vue
@@ -3,16 +3,16 @@
     <div class="navbar">
 
       <div class="nav-left">
-        <div @click="home" class="navbar-button">
+        <div @click="home" class="stile-button">
           <span class="navbar-text">Auction</span>
         </div>
       </div>
 
       <div class="nav-right">
-        <div @click="registration" class="margin-left-20 navbar-button">
+        <div @click="registration" class="margin-left-20 stile-button">
           <span class="navbar-text">Registration</span>
         </div>
-        <div @click="login" class="margin-left-20 navbar-button">
+        <div @click="login" class="margin-left-20 stile-button">
           <span class="navbar-text">Login</span>
         </div>
       </div>
@@ -55,7 +55,8 @@ export default {
   position: fixed;
   width: 100%;
   height: 60px;
-  background-color: #6EAE97;
+  background: #59cf59;
+  background: linear-gradient(90deg, rgba(89, 207, 89, 1) 0%, rgba(87, 199, 133, 1) 50%, rgba(139, 237, 83, 1) 100%);
 }
 .nav-left {
   padding: 10px;
@@ -75,12 +76,21 @@ export default {
 .margin-left-20 {
   margin-left: 20px;
 }
-.navbar-button {
+.stile-button {
   cursor: pointer;
-  background-color: #28a745;
-  border-color: #28a745;
-  border: 1px solid transparent;
-  padding: 10px;
+  background: linear-gradient(90deg, rgba(9, 133, 9, 1) 0%, rgba(20, 143, 71, 1) 50%, rgba(72, 179, 14, 1) 100%);
+  border: none;
+  color: white;
+  padding: 12px;
   border-radius: 6px;
+  font-size: 16px;
+  transition: background-color 0.3s ease, transform 0.2s ease;
+}
+.stile-button:hover {
+  background-color: #28a745;
+  transform: translateY(-2px);
+}
+.stile-button:focus {
+  outline: none;
 }
 </style>
--- web/src/views/Registration.vue
+++ web/src/views/Registration.vue
@@ -1,11 +1,25 @@
 <template>
-  <div>
-    <span>Registration</span>
+  <div class="outer-container">
+    <div class="registration-field">
+      <h2>Регистрация</h2>
+      <div class="input-column outer-container">
+        <input type="text" placeholder="Введите ваше имя" />
+      </div>
+      <div class="input-column outer-container">
+        <input type="email" placeholder="Введите ваш email" />
+      </div>
+      <div class="input-column outer-container">
+        <input type="password" placeholder="Введите ваш пароль" />
+      </div>
+      <div class="input-column outer-container">
+        <input type="confirm password" placeholder="Повторите ваш пароль" />
+      </div>
+        <button class="register-button">Зарегистрироваться</button>
+    </div>
   </div>
 </template>
 
 <script>
-
 export default {
   components: {
   },
@@ -26,5 +40,48 @@ export default {
 </script>
 
 <style scoped>
+.outer-container {
+  display: flex;
+  justify-content: center;
 
+}
+.registration-field {
+  margin-top: 110px;
+  width: 400px;
+  height: 400px;
+  border-radius: 50px;
+  padding: 20px;
+  background: linear-gradient(90deg, rgba(89, 207, 89, 1) 0%, rgba(87, 199, 133, 1) 50%, rgba(139, 237, 83, 1) 100%);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.input-column {
+  width: 75%;
+  margin-bottom: 15px;
+}
+.input-column input {
+  width: 90%;
+  padding: 15px;
+  border: none;
+  border-radius: 15px;
+  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+}
+.register-button {
+  cursor: pointer;
+  background: linear-gradient(90deg, rgba(9, 133, 9, 1) 0%, rgba(20, 143, 71, 1) 50%, rgba(72, 179, 14, 1) 100%);
+  border: none;
+  color: white;
+  padding: 12px;
+  border-radius: 6px;
+  font-size: 16px;
+  transition: background-color 0.3s ease, transform 0.2s ease;
+}
+.register-button:hover {
+  background-color: #28a745;
+  transform: translateY(-2px);
+}
+.register-button:focus {
+  outline: none;
+}
 </style>
